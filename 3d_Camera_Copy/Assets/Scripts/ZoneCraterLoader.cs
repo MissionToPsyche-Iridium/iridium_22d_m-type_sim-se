99fc0dd8e4e160fcc59146bf8028a48a6e804ebd
tmcrawf4 <tristancrawford1@gmail.com>
2024-11-03 13:43:59 -0500

    new script to load crater zone

---
+++ 3d_Camera_Copy/Assets/Scripts/ZoneCraterLoader.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ZoneCraterLoader : MonoBehaviour
+{
+    public string zoneSceneName = "CraterZone";  // Name of the scene to load
+
+    void OnMouseDown()
+    {
+        LoadZone();
+    }
+
+    // Method to load the specified scene
+    public void LoadZone()
+    {
+        if (!string.IsNullOrEmpty(zoneSceneName))
+        {
+            SceneManager.LoadScene(zoneSceneName, LoadSceneMode.Single);
+            Debug.Log($"Scene '{zoneSceneName}' loaded.");
+        }
+        else
+        {
+            Debug.LogError("Zone scene name is not set.");
+        }
+    }
+}
+
