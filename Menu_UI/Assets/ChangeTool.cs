4d3cd6f228223b3647fb92a96856108adf418521
Daryl Kyle <gdn015@gmail.com>
2024-11-15 11:17:18 -0800

    add ability to hide/show Chimra turret by pressing C

--- Menu_UI/Assets/ChangeTool.cs
+++ Menu_UI/Assets/ChangeTool.cs
@@ -1,29 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeTool : MonoBehaviour
 {
-    //RoverTool currentTool;
-    //Animator animator;
-    MeshSockets sockets;
+    public GameObject chimraTool;
+
     // Start is called before the first frame update
     void Start()
     {
-        //animator = GetComponent<Animator>();
-        sockets = GetComponent<MeshSockets>();
-        
+
     }
-    /**
-    public void changeTool(RoverTool tool)
-    {
-        currentTool = tool;
-        sockets.Attach(tool.transform, MeshSockets.SocketId.Arm);
-    }*/
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (chimraTool.activeSelf == true)
+            {
+                chimraTool.SetActive(false);
+            }
+            else
+            {
+                chimraTool.SetActive(true);
+            }
+        }
     }
 }
