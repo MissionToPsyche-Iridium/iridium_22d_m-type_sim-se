7cc81b08d93836fe7ea6efcb54dcfa07022beaa7
Daryl Kyle <gdn015@gmail.com>
2024-11-17 20:05:53 -0800

    add method to equip claw tool

--- Menu_UI/Assets/ChangeTool.cs
+++ Menu_UI/Assets/ChangeTool.cs
@@ -7,6 +7,7 @@ public class ChangeTool : MonoBehaviour
 {
     public GameObject chimraTool;
     public GameObject archScrew;
+    public GameObject clawTool;
 
     // Start is called before the first frame update
     void Start()
@@ -20,19 +21,36 @@ public class ChangeTool : MonoBehaviour
         //testing keybinds
         if (Input.GetKeyDown(KeyCode.C))
         {
-            equipChimra();
+            if (chimraTool.activeSelf == false)
+            {
+                equipChimra();
+            } else
+            {
+                equipClaw();
+            }
         } else if (Input.GetKeyDown(KeyCode.T)) {
             equipArchScrew();
         }
     }
+
+    private void equipClaw()
+    {
+        chimraTool.SetActive(false);
+        archScrew.SetActive(false);
+        clawTool.SetActive(true);
+    }
+
     public void equipChimra()
     {
         chimraTool.SetActive(true);
         archScrew.SetActive(false);
+        clawTool.SetActive(false);
+
     }
     public void equipArchScrew()
     {
         chimraTool.SetActive(false);
         archScrew.SetActive(true);
+        clawTool.SetActive(false);
     }
 }
