6357375bf073664c9dd94546db3ef154595b268c
mives2 <mives458@gmail.com>
2024-11-22 01:31:12 -0600

    Implement Mouse Hover Functionality to Slide in Zone Title

--- Menu_UI/Assets/Scripts/ZoneHoverAnimation.cs
+++ Menu_UI/Assets/Scripts/ZoneHoverAnimation.cs
@@ -5,19 +5,15 @@ using UnityEngine;
 
 public class ZoneHoverAnimation : MonoBehaviour
 {
-    public Camera mainCamera;
     public Animator animator;
-    public float height = 1.0f;
 
     public void OnMouseEnter()
     {
-        Debug.Log("Mouse Detected!");
         animator.SetBool("MouseOver", true);
     }
-
+    
     public void OnMouseExit()
     {
-        Debug.Log("Bye!");
         animator.SetBool("MouseOver", false);
     }
 }
