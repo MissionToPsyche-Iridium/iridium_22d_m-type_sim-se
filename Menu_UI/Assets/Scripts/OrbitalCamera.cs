a2a226e562d80ca422e9473444d80bb73c24243b
tmcrawf4 <tristancrawford1@gmail.com>
2025-01-14 21:47:36 -0500

    minor changes

--- Menu_UI/Assets/Scripts/OrbitalCamera.cs
+++ Menu_UI/Assets/Scripts/OrbitalCamera.cs
@@ -19,16 +19,18 @@ public class OrbitalCamera : MonoBehaviour
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        float horizontal = Input.GetAxis("Horizontal"); 
         float vertical = Input.GetAxis("Vertical");
 
         if (horizontal != 0)
         {
+            // Rotates the camera horizontally around the target object (Using A/D keys)
             transform.RotateAround(target.position, Vector3.up, horizontal * rotationSpeed * Time.deltaTime);
         }
 
         if (vertical != 0)
         {
+            // Rotates the camera vertically around the target (Using W/S keys)
             transform.RotateAround(target.position, transform.right, -vertical * rotationSpeed * Time.deltaTime);
         }
 
