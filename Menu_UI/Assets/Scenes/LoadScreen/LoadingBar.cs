3953c102f38da50356f3e0441e2ba638aa01f40a
tmcrawf4 <tristancrawford1@gmail.com>
2025-01-25 02:45:03 -0500

    updated loading bar script

--- Menu_UI/Assets/Scenes/LoadScreen/LoadingBar.cs
+++ Menu_UI/Assets/Scenes/LoadScreen/LoadingBar.cs
@@ -3,29 +3,29 @@ using UnityEngine.UI;
 
 public class LoadingBar : MonoBehaviour
 {
-    public Slider loadingBar; 
+    private Slider loadingBar; 
     public float loadingSpeed = 0.5f; 
 
     private float progress = 0f; 
 
     void Start()
     {
+        loadingBar = GetComponent<Slider>();
+
         if (loadingBar != null)
         {
             loadingBar.value = 0f;
+            StartCoroutine(LoadingProcess());
         }
     }
 
-    void Update()
+    System.Collections.IEnumerator LoadingProcess()
     {
-        if (loadingBar != null && loadingBar.value < progress)
+        while (loadingBar.value < loadingBar.maxValue)
         {
             loadingBar.value += loadingSpeed * Time.deltaTime;
+            yield return null; 
         }
-    }
 
-    public void SetProgress(float progress)
-    {
-        progress = Mathf.Clamp(progress, 0f, 1f);
     }
 }
