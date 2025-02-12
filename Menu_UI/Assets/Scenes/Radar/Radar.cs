6f5f50bb53f276a97a5795aa408fe7a773f4b68a
tmcrawf4 <tristancrawford1@gmail.com>
2025-02-12 01:09:37 -0500

    initial radar script

---
+++ Menu_UI/Assets/Scenes/Radar/Radar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    public GameObject[] trackedObjects;
+    List<GameObject> radarObj;
+    public GameObject radarPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createRadarObjects() 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void createRadarObjects()
+    {
+        radarObj = new List<GameObject>();
+        foreach (GameObject o in radarObj)
+        {
+            GameObject x = Instantiate(radarPrefab, o.transform.position, Quaternion.identity) as GameObject;
+            radarObj.Add(x);
+        }
+    }
+}
