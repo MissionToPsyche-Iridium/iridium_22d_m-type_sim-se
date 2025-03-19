565fb1c44fbf556883699a841a921a96c1696042
tmcrawf4 <tristancrawford1@gmail.com>
2025-03-19 00:19:42 -0400

    script for the flat zone

---
+++ Menu_UI/Assets/Scripts/SelectorButton4.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectorButton4 : MonoBehaviour
+{
+    public string zoneSceneName = "FlatSurface";
+
+    void OnMouseDown()
+    {
+        LoadLoadingScreen();
+    }
+
+    public void LoadLoadingScreen()
+    {
+        SceneManager.LoadScene("LoadingScreen");
+        
+    }
+    public void NextScene()
+    {
+        SceneManager.LoadScene("FlatSurface");
+    }
+}
