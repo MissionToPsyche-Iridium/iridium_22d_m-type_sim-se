59576681d322ee2680625018e4658ed552aa6aac
tmcrawf4 <tristancrawford1@gmail.com>
2025-03-28 01:15:29 -0400

    script for text hovering over rock

---
+++ Menu_UI/Assets/Scripts/FloatingText.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingText : MonoBehaviour
+{
+    Transform mainCam;
+    Transform rock;
+    Transform sceneCanvas;
+
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main.transform;
+        rock = transform.parent;
+        sceneCanvas = GameObject.FindObjectOfType<Canvas>().transform;
+
+        transform.SetParent(sceneCanvas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - mainCam.transform.position);
+        transform.position = rock.position + offset;
+    }
+}
